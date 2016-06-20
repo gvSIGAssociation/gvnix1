29ea46d556527a7963b843c5fb68545f1ce4ef0b
David Cervera <dcervera@disid.com>
2016-06-20 13:31:28 +0200

    Uploaded 'roo' directory.

---
+++ roo/addon-web-selenium/src/main/java/org/springframework/roo/addon/web/selenium/SeleniumCommands.java
@@ -0,0 +1,48 @@
+package org.springframework.roo.addon.web.selenium;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.shell.CliAvailabilityIndicator;
+import org.springframework.roo.shell.CliCommand;
+import org.springframework.roo.shell.CliOption;
+import org.springframework.roo.shell.CommandMarker;
+
+/**
+ * Commands for the 'selenium' add-on to be used by the ROO shell.
+ * 
+ * @author Stefan Schmidt
+ * @author Juan Carlos García
+ * @since 1.0
+ */
+@Component
+@Service
+public class SeleniumCommands implements CommandMarker {
+
+    @Reference private SeleniumOperations seleniumOperations;
+    
+    @CliAvailabilityIndicator({ "selenium all", "selenium test" })
+    public boolean isJdkFieldManagementAvailable() {
+        return seleniumOperations.isSeleniumInstallationPossible();
+    }
+
+    @CliCommand(value = "selenium test", help = "Creates a new Selenium test for a particular controller")
+    public void generateTest(
+            @CliOption(key = "controller", mandatory = true, help = "Controller to create a Selenium test for") final JavaType controller,
+            @CliOption(key = "name", mandatory = false, help = "Name of the test") final String name,
+            @CliOption(key = "serverUrl", mandatory = false, unspecifiedDefaultValue = "http://localhost:8080/", specifiedDefaultValue = "http://localhost:8080/", help = "URL of the server where the web application is available, including protocol, port and hostname") final String url) {
+
+        seleniumOperations.generateTest(controller, name, url);
+    }
+    
+    @CliCommand(value = "selenium all", help = "Creates a Selenium tests for all controllers")
+    public void generateAll(
+    		@CliOption(key = "serverUrl", mandatory = false, unspecifiedDefaultValue = "http://localhost:8080/", specifiedDefaultValue = "http://localhost:8080/", help = "URL of the server where the web application is available, including protocol, port and hostname") final String url) {
+
+        seleniumOperations.generateAll(url);
+    }
+
+
+
+}
---
+++ roo/addon-web-selenium/src/main/java/org/springframework/roo/addon/web/selenium/SeleniumOperations.java
@@ -0,0 +1,31 @@
+package org.springframework.roo.addon.web.selenium;
+
+import org.springframework.roo.model.JavaType;
+
+/**
+ * Provides Selenium operations.
+ * 
+ * @author Ben Alex
+ * @author Juan Carlos García
+ * @since 1.0
+ */
+public interface SeleniumOperations {
+
+    /**
+     * Creates a new Selenium testcase
+     * 
+     * @param controller the JavaType of the controller under test (required)
+     * @param name the name of the test case (optional)
+     * @param serverURL the URL of the Selenium server (optional)
+     */
+    void generateTest(JavaType controller, String name, String serverURL);
+    
+    /**
+     * Creates Selenium testcase for all registered controllers
+     * 
+     * @param serverURL the URL of the Selenium server (optional)
+     */
+    void generateAll(String serverURL);
+
+    boolean isSeleniumInstallationPossible();
+}
